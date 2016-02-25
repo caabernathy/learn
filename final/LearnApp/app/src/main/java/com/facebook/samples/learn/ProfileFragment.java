c4b7a8c362f277b5bf296f730bc3f2dd63543637
Christine Abernathy <ca.abernathy@fb.com>
2016-02-25 01:22:10 +0100

    Set up the login

--- final/LearnApp/app/src/main/java/com/facebook/samples/learn/ProfileFragment.java
+++ final/LearnApp/app/src/main/java/com/facebook/samples/learn/ProfileFragment.java
@@ -1,15 +1,26 @@
 package com.facebook.samples.learn;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 /**
  * Fragment for handing Profile view
  */
 public class ProfileFragment extends Fragment {
+
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -23,6 +34,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        callbackManager = CallbackManager.Factory.create();
 
     }
 
@@ -31,7 +43,36 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        loginButton = (LoginButton) view.findViewById(R.id.login_button);
+        loginButton.setFragment(this);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // Successful login
+                Log.d("ProfileFragment", "Login success");
+            }
+
+            @Override
+            public void onCancel() {
+                // Login canceled
+                Log.d("ProfileFragment", "Login canceled");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // Error during login
+                Log.d("ProfileFragment", "Login error");
+            }
+        });
+
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
 }
